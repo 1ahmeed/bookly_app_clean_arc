d7ac7d403b7edc58ff922623f628782012c3d0ab
1ahmeed <ahmedtarekat2338@gmail.com>
2023-06-13 20:45:43 +0300

    add functions to api services

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -1,18 +1,51 @@
 import 'package:dio/dio.dart';
 
 class ApiService {
-
-  final _baseUrl='https://www.googleapis.com/books/v1/';
+  final _baseUrl = 'https://www.googleapis.com/books/v1/';
   final Dio _dio;
 
   ApiService(this._dio);
 
- Future<Map<String,dynamic>>? get({
-  required String endPoint
-})async{
- var response= await _dio.get("$_baseUrl$endPoint");
- return response.data;
-}
+  Future<Map<String, dynamic>>? get({
+    required String endPoint,
+    // String lang = "en",
+    // String? token,
+  }) async {
+    // _dio.options.headers={
+    //   "lang":lang,
+    //   "authorization":token,
+    // };
+    var response = await _dio.get("$_baseUrl$endPoint");
+    return response.data;
+  }
+
+  Future<Map<String, dynamic>>? post({
+    required String endPoint,
+    required Map<String, dynamic> body,
+    // String lang = "en",
+    // String? token,
+  }) async {
+    // _dio.options.headers={
+    //   "lang":lang,
+    //   "authorization":token,
+    // };
+    var response = await _dio.post("$_baseUrl$endPoint", data: body);
+    return response.data;
+  }
+
+  Future<Map<String, dynamic>>? put({
+    required String endPoint,
+    required Map<String, dynamic> body,
+    // String lang = "en",
+    // String? token,
+  }) async {
+    // _dio.options.headers={
+    //   "lang":lang,
+    //   "authorization":token,
+    // };
+    var response = await _dio.put("$_baseUrl$endPoint", data: body);
+    return response.data;
+  }
 
 
 }
